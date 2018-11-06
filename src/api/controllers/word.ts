269355fde57c3213a3a8878e92080191bfbd5d1f
joshmccall <josh@wewritecode.com>
2018-11-05 20:02:18 -0600

    added create route for wordcontroller

--- src/api/controllers/word.ts
+++ src/api/controllers/word.ts
@@ -1,3 +1,5 @@
 export class WordController {
-    
+    public create (): void {
+
+    }
 }
--- src/api/routes.ts
+++ src/api/routes.ts
@@ -1,12 +1,17 @@
 import { Request, Response } from 'express';
+import { WordController } from './controllers/word';
 
 export class RouteProvider {
+    private wordController: WordController = new WordController();
     public routes (app): void {
+
         app.route('/')
             .get((req: Request, res: Response) => {
                 res.status(200).send({
                     message: 'GET request successfulll!!!!'
                 })
-            })  
+            });
+        app.route('word')
+            .post(this.wordController.create);
     }
 }
