f2f8e5b9d357199d949bbb2b9af1c80f8750e554
joshmccall <josh@wewritecode.com>
2018-11-05 19:30:24 -0600

    basic express setup in place

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,15 @@
 import * as express from 'express';
 import * as bodyParser from 'body-parser';
+import { RouteProvider } from './api/routes';
 
 class App {
     public app: express.Application;
+    public routeProvider: RouteProvider = new RouteProvider();
 
     constructor () {
         this.app = express();
         this.config();
+        this.routeProvider.routes(this.app);
     }
 
     private config (): void {
