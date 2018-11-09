85e675284ed45f038c515a53a28bf8ec56706869
joshmccall <josh@wewritecode.com>
2018-11-09 11:51:52 -0600

    added flex layout to app module, tweaked router outlet to be fullscreen

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 
 import { AppRoutes } from './app.routing';
 
@@ -13,6 +15,7 @@ import { UnauthenticatedModule } from './unauthenticated/unauthenticated.module'
   ],
   imports: [
     BrowserModule,
+    FlexLayoutModule,
     FontAwesomeModule,
     UnauthenticatedModule,
     AppRoutes
